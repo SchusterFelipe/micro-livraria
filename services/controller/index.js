744e2e88dbf8544df431f44eb11eab08e1db941d
SchusterFelipe <164955577+SchusterFelipe@users.noreply.github.com>
2025-04-01 20:23:22 -0300

    Update index.js

--- services/controller/index.js
+++ services/controller/index.js
@@ -57,5 +57,5 @@ app.get('/product/:id', (req, res, next) => {
  * Inicia o router
  */
 app.listen(3000, () => {
-    console.log('Controller Service running on http://localhost:3000');
+    console.log('Controller Service running on http://127.0.0.1:3000');
 });
